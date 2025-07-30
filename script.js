737971593d67f070dc66b32a06cc07ff587d73ef
fguzman717 <felixguzman717@gmail.com>
2025-07-29 20:09:18 -0500

    Added the Logic for Game to Function on Console

--- script.js
+++ script.js
@@ -30,6 +30,65 @@ const player2 = {
   },
 };
 
-console.log(gameBoard);
-console.log(player1.playMarker(0));
-console.log(player2.playMarker(5));
+function gameCheck() {
+  const board = gameBoard.board;
+  const winPatterns = [
+    // rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonals
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winPatterns.length; i++) {
+    let pattern = winPatterns[i];
+    let a = pattern[0];
+    let b = pattern[1];
+    let c = pattern[2];
+
+    if (board[a] !== null && board[a] === board[b] && board[a] === board[c]) {
+      return { winner: board[a], pattern: pattern };
+    }
+  }
+
+  // If there is no winner, returns tie
+  let allFilled = true;
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === null) {
+      allFilled = false;
+      break;
+    }
+  }
+  if (allFilled) {
+    return { tie: true };
+  }
+
+  // Game is still not over
+  return null;
+}
+
+// player1.playMarker(0);
+// console.log("Game Status: ", gameCheck());
+
+// player1.playMarker(1);
+// console.log("Game Status: ", gameCheck());
+
+// player1.playMarker(2);
+// console.log("Game Status: ", gameCheck());
+
+// player2.playMarker(3);
+// console.log("Game Status: ", gameCheck());
+
+// player2.playMarker(4);
+// console.log("Game Status: ", gameCheck());
+
+// player2.playMarker(5);
+// console.log("Game Status: ", gameCheck());
+
+// console.log(gameBoard);
